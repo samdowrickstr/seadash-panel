1d5e4410226741c694164d4cc5e9d23928004edd
samdowrickstr <sam.dowrick@str-subsea.com>
2025-07-30 13:32:58 +0100

    update cmake

--- out/build/debug/CMakeFiles/clean_additional.cmake
+++ out/build/debug/CMakeFiles/clean_additional.cmake
@@ -5,12 +5,6 @@ if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
   file(REMOVE_RECURSE
   "seadash-panel\\CMakeFiles\\seadash-panel_app_autogen.dir\\AutogenUsed.txt"
   "seadash-panel\\CMakeFiles\\seadash-panel_app_autogen.dir\\ParseCache.txt"
-  "seadash-panel\\CMakeFiles\\seadash-panel_autogen.dir\\AutogenUsed.txt"
-  "seadash-panel\\CMakeFiles\\seadash-panel_autogen.dir\\ParseCache.txt"
-  "seadash-panel\\CMakeFiles\\seadash-panelplugin_autogen.dir\\AutogenUsed.txt"
-  "seadash-panel\\CMakeFiles\\seadash-panelplugin_autogen.dir\\ParseCache.txt"
   "seadash-panel\\seadash-panel_app_autogen"
-  "seadash-panel\\seadash-panel_autogen"
-  "seadash-panel\\seadash-panelplugin_autogen"
   )
 endif()
---
+++ out/build/debug/seadash-panel/.qt/seadash-panel_app_res.cmake
@@ -0,0 +1,7 @@
+
+set(target "seadash-panel_app")
+set(working_dir "C:/Users/Sam.Dowrick/Documents/GitHub/seadash-panel/seadash-panel")
+set(src_and_dest_list
+
+)
+set(timestamp_file "C:/Users/Sam.Dowrick/Documents/GitHub/seadash-panel/out/build/debug/seadash-panel/.qt/seadash-panel_app_res.txt")
